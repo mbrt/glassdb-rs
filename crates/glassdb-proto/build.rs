329ca6c4d5dfb2d2e0594cdc21534ba943c64085
mbrt <405934+mbrt@users.noreply.github.com>
2026-06-09 13:26:58 +0200

    Avoid customers needing protoc
    Commit the generated code to make that easier for customers.
    
    Also add a CI check to make sure it's in sync.

--- crates/glassdb-proto/build.rs
+++
@@ -1,7 +0,0 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    println!("cargo:rerun-if-changed=proto/transaction.proto");
-    prost_build::compile_protos(&["proto/transaction.proto"], &["proto"])?;
-    Ok(())
-}
